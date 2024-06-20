a697840d6622bb6e0f53187633438e516f24e2bf
Daniel Chin <DanChin327@gmail.com>
2024-06-20 15:27:34 -0600

    adding storage ability

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+//function stores data
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+
+//add new note function
 createBtn.addEventListener('click', ()=>{
   let inputBox = document.createElement('p');
   let img = document.createElement("img")
@@ -11,8 +25,18 @@ createBtn.addEventListener('click', ()=>{
   notesContainer.appendChild(inputBox).appendChild(img);
 })
 
+//delete function
 notesContainer.addEventListener("click", function(e) {
   if(e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  }
+  else if(e.target.tagName === "P") {
+    notes = document.querySelectorAll("input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function() {
+          updateStorage();
+      }
+    })
   }
 })
